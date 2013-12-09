0dfa8a034d3fdff25cabb224217bf51137f3f74b
Greg Hale <imalsogreg@gmail.com>
2013-12-09 10:28:10 -0500

    field tag validation

--- src/handlers/HandleNewPaper.hs
+++ src/handlers/HandleNewPaper.hs
@@ -21,16 +21,27 @@ import           Application
 handleNewPaper :: Handler App App ()
 handleNewPaper = undefined
 
-documentForm :: Maybe User -> [DocClass] -> Form Text m Document
-documentForm fromUser docClasses =
+documentForm :: Maybe User -> [DocClass] -> [FieldTag] -> Form Text m Document
+documentForm fromUser allDocClasses allDocTags =
   Document fromUser
   <$> "title"    .: check "Not a valid title" (not T.empty) (text Nothing)
   <*> "authors"  .: check "Invalid authors"  validateAuthors (text Nothing)
   <*> "link"     .: check "Not a valid link" (not T.empty) (text Nothing)
-  <*> "docClass" .: choice docClasses Nothing
-  <*> "
+  <*> "docClass" .: choice allDocClasses Nothing
+  <*> "docTags"  .: check "Invalid tags" (validateTags allDocTags) (text Nothing)
 
 validateAuthors :: Text -> Result Text [Text]
 validateAuthors authorsText
   | T.null authorsText = Error "Authors required"
   | otherwise          = Success $ T.splitOn "," authorsText
+
+validateTags :: [FieldTag] -> Text -> Result Text [FieldTag]
+validateTags allTags' formTags'
+  | all (`elem` allTags) formTags =
+    Success . map FieldTag $ formTags
+  | otherwise =
+    Error $ T.concat ["Unrecognized tags: " ,
+    (T.unwords . filter (`notElem` allTags) $ formTags)]
+  where
+    allTags  = map (\(FieldTag t) -> t) allTags' 
+    formTags = T.words formTags'
