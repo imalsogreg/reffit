da62be8ef683f566e545be6ff05f80728c47e5c3
Greg Hale <ImAlsoGreg@gmail.com>
2014-01-05 12:27:05 -0500

    may be close on a new approach to catching logout-during-writing case

--- src/handlers/HandleNewCritique.hs
+++ src/handlers/HandleNewCritique.hs
@@ -32,8 +32,9 @@ import           Control.Monad.Trans
 import qualified Data.Map                     as Map
 import           Data.Text                    (Text)
 import qualified Data.Text                    as T
+import           Data.Maybe (fromMaybe)
 import           Data.Time
-import           Data.Text.Encoding (decodeUtf8)
+import           Data.Text.Encoding (decodeUtf8, encodeUtf8)
 import           Text.Digestive
 import           Text.Digestive.Blaze.Html5
 import qualified Text.Blaze.Html5             as H
@@ -45,8 +46,8 @@ import           Heist
 import           Snap.Snaplet.Heist
 import qualified Heist.Interpreted as I
 
-newOCommentForm :: (Monad m) => User -> OverviewCommentType -> UTCTime -> Form Text m OverviewComment
-newOCommentForm formUser ocType t =
+newOCommentForm :: (Monad m) => UserName -> OverviewCommentType -> UTCTime -> Form Text m OverviewComment
+newOCommentForm formUserName ocType t =
   OverviewComment
   <$> "poster"    .: choice posterOpts Nothing
   <*> "prose"     .: check "Not a valid entry" (not . T.null) (text Nothing)
@@ -54,7 +55,7 @@ newOCommentForm formUser ocType t =
   <*> pure []
   <*> pure t 
   where
-    posterOpts = [(Just (userName formUser), userName formUser)
+    posterOpts = [(Just formUserName, formUserName)
                  ,(Nothing, "Anonymous")]
     dimOpts = [(Novelty,"Novelty"),(Rigor,"Rigor"),(Coolness,"Coolness")]
     ocVoteVal = case ocType of
@@ -95,31 +96,50 @@ newCritiqueView view = do
 newOCommentView :: View H.Html -> H.Html
 newOCommentView = undefined
 
-{-
-handleNewCritique :: UpDownVote -> Handler App (AuthManager App) ()  
-handleNewCritique critVal = do 
-  userMap   <- query QueryAllUsers
+handleNewOComment :: OverviewCommentType -> T.Text -> Handler App (AuthManager App) ()
+handleNewOComment commentType defaultUsername = do
+  userMap <- query QueryAllUsers
   pId'      <- getParam "paperid"
   authUser' <- currentUser
   t         <- liftIO $ getCurrentTime
-  case join $ readMay . T.unpack . decodeUtf8 <$> pId' of  
-    Nothing -> writeText "paperid error" --TODO proper error message
-    Just pId -> 
+  case join $ readMay . T.unpack . decodeUtf8 <$> pId' of
+    Nothing -> writeText "Server error - couldn't find paper in database"
+    Just pId -> do
+      let user' = join $ (Map.lookup <$> (userLogin <$> authUser') <*> pure userMap)
+          userN = maybe defaultUsername userName user'
+       case user' of 
+         Nothing -> writeText $ "handleNewOComment - didn't find user in database"
+         Just _ -> do
+           (vw,rs) <- runForm "newOCommentForm" $ newOCommentForm userN commentType t -- What is this?
+           case (user', rs) of
+             (Nothing,Nothing) -> writeText "Server - tried to render form but no user"
+             (Just _ ,Nothing) -> do
+               heistLocal (bindDigestiveSplices vw) $
+                 renderWithSplices "new_o_comment" (oCommentFormSplices commentType)                  
+             (Nothing, Just comment) -> writeText "Server - caught logout case!"
+             (Just _ , Just comment) -> do
+               let posterName = maybe Nothing (const $ Just userN) (ocPoster comment)
+               _ <- update $ AddOComment posterName pId' comment 
+               redirect . BS.pack $ "/view_article/" ++ show pId
+ 
+  {-
+    Just pId ->
       case join $ (Map.lookup <$> (userLogin <$> authUser') <*> pure userMap) of
-        Nothing -> writeBS "didn't find user in database"
+
         Just user -> do
-          (vw,rs) <- runForm "newCritiqueForm" $ newCritiqueForm user critVal t
-          case rs of 
-            Just critique -> do
-              let user' = maybe Nothing (const $ Just user) (critiquePoster critique)
-              _ <- update $ AddCritique user' pId critique 
-              --let a = sId :: SummaryId  -- TODO: Must vote for
-              -- Vote for own summary     -- user's summary automatically
+          (vw,rs) <- runForm "newOCommentForm" $ newOCommentForm user commentType t -- What is this?
+          case rs of
+            Just comment -> do
+              let user' = maybe Nothing (const $ Just user) (ocPoster comment)
+              _ <- update $ AddOComment user' pId comment 
               redirect . BS.pack $ "/view_article/" ++ show pId
-            Nothing -> do 
-              heistLocal (bindDigestiveSplices vw) $ render "new_critique"
--}
-  
+            Nothing -> do
+              heistLocal (bindDigestiveSplices vw) $
+                renderWithSplices "new_o_comment" (oCommentFormSplices commentType)                  
+                  
+    -}              
+               
+{- This isn't working - I get "Post - got Nothing".  Don't understand digestive well enough
 handleNewOComment :: OverviewCommentType -> Handler App (AuthManager App) ()
 handleNewOComment commentType = do
   userMap <- query QueryAllUsers
@@ -141,6 +161,31 @@ handleNewOComment commentType = do
           method GET handleRenderForm <|> method POST handlePostForm
             where 
               handleRenderForm = do
+                view <- getForm  "newOCommentForm" $ newOCommentForm user commentType t
+                heistLocal (bindDigestiveSplices view) $
+                  renderWithSplices "new_o_comment" (oCommentFormSplices commentType)
+--                return ()
+              handlePostForm   = do
+                (v,r) <- postForm "newOCommentForm" (newOCommentForm user commentType t) (snapEnv [])
+                case r of
+                  Nothing -> writeText "Post got Nothing"
+                  Just _  -> writeText $ T.append "Post got something. pId: " (T.pack . show $ (pId :: DocumentId))
+                return ()
+                
+
+snapEnv :: MonadSnap m => [(T.Text, FilePath)] -> Env m
+snapEnv allFiles path = do
+    inputs <- map (TextInput . decodeUtf8) . findParams <$> getParams
+    let files = map (FileInput . snd) $ filter ((== name) . fst) allFiles
+    return $ inputs ++ files
+  where
+    findParams = fromMaybe [] . Map.lookup (encodeUtf8 name)
+    name       = fromPath path
+-} 
+
+
+              {-
+              handleRenderForm = do
                 (vw,rs) <- runForm "newOCommentForm" $ newOCommentForm user commentType t
                 case rs of 
                   Nothing -> do
@@ -155,7 +200,7 @@ handleNewOComment commentType = do
                     _ <- update $ AddOComment user' pId comment 
                     redirect . BS.pack $ "/view_article/" ++ show pId
                   Nothing -> writeText "server error - impossible case in handleNewOComment handlePostForm"
-
+-}
 
 oCommentFormSplices :: Monad m => OverviewCommentType -> Splices (I.Splice m)
 oCommentFormSplices Summary' = "reBlock" ## I.textSplice ""
