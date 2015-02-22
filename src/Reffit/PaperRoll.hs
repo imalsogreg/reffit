e6b5d185e7192174fa08d64de2cdafd83e597a94
Greg Hale <imalsogreg@gmail.com>
2015-02-22 15:26:22 -0500

    stub

--- src/Reffit/PaperRoll.hs
+++ src/Reffit/PaperRoll.hs
@@ -26,6 +26,7 @@ import qualified Heist.Interpreted as I
 import           Application
 import           Reffit.AcidTypes
 import           Reffit.Document
+import           Reffit.Handlers.Document
 import           Reffit.FieldTag
 import           Reffit.Scores
 import           Reffit.Search
@@ -74,8 +75,15 @@ allPaperRollSplices :: [Document] -> Splices (SnapletISplice App)
 allPaperRollSplices docs = do
   "paper_roll_papers" ## (renderPaperRollPapers (take 100 docs))
 
+allDocOverviewSplices :: [Int] -> Splices (SnapletISplice App)
+allDocOverviewSplices inds = do
+  "paper_roll_papers" ## (renderDocOverviewSplices (take 100 inds))
+
 renderPaperRollPapers :: [Document] -> SnapletISplice App
 renderPaperRollPapers = I.mapSplices $ I.runChildrenWith . splicesFromDocument
+
+renderDocOverviewPapers :: [Int] -> SnapletISplice App
+renderDocOverviewPapers = I.mapSplices $ I.runChildrenWith . splicesFromDocOverview
 
 splicesFromDocument :: Document -> Splices (SnapletISplice App)
 splicesFromDocument doc = do
